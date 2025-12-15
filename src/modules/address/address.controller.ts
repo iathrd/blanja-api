9f672b2b5e72d9e7869c31da0af61cd8398f1f12
Iqbal Athorid <iqbalathoridd@gmail.com>
2025-12-15 22:54:50 +0700

    feat: create update address api

--- src/modules/address/address.controller.ts
+++ src/modules/address/address.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Param, Post, Put } from '@nestjs/common';
 import { AddressService } from './address.service';
 import { CreateAddressDto } from './dto/create-address.dto';
+import { UpdateAddressDto } from './dto/update-address.dto';
 
 @Controller('address')
 export class AddressController {
@@ -9,5 +10,13 @@ export class AddressController {
   @Post()
   createAddress(@Body() createAddressDto: CreateAddressDto) {
     return this.addressService.createAddress(createAddressDto);
+  }
+
+  @Put(':id')
+  updateAddress(
+    @Param('id') id: string,
+    @Body() updateAddressDto: UpdateAddressDto,
+  ) {
+    return this.addressService.updateAddress(+id, updateAddressDto);
   }
 }
--- src/modules/address/address.service.ts
+++ src/modules/address/address.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateAddressDto } from './dto/create-address.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Address } from './entities/address.entity';
 import { Repository } from 'typeorm';
+import { UpdateAddressDto } from './dto/update-address.dto';
 
 @Injectable()
 export class AddressService {
@@ -12,16 +13,24 @@ export class AddressService {
   ) {}
 
   async createAddress(createAddressDto: CreateAddressDto) {
-    const address = new Address();
-    address.address = createAddressDto.address;
-    address.district = createAddressDto.district;
-    address.phone_number = createAddressDto.phone_number;
-    address.province = createAddressDto.province;
-    address.sub_district = createAddressDto.sub_district;
-    address.postal_code = createAddressDto.postal_code;
+    const address = this.addressRepository.create(createAddressDto);
+    return await this.addressRepository.save(address);
+  }
 
-    await this.addressRepository.save(address);
+  async updateAddress(
+    id: number,
+    updateAddressDto: UpdateAddressDto,
+  ): Promise<Address> {
+    const address = await this.addressRepository.findOne({
+      where: { id },
+    });
 
-    return address;
+    if (!address) {
+      throw new NotFoundException('Address not found');
+    }
+
+    Object.assign(address, updateAddressDto);
+
+    return await this.addressRepository.save(address);
   }
 }
